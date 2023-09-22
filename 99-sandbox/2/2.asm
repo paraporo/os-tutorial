68a69afad4c5cd3e849498c19cd9c79eb7d9879d
Ahmet Erdem <56089895+paraporo@users.noreply.github.com>
2023-09-22 22:06:40 +0300

    commit

--- 99-sandbox/2/2.asm
+++ 99-sandbox/2/2.asm
@@ -6,7 +6,7 @@ bArr resb 100
 wArr resw 3000
 dwArr resd 200
 qArr resq 5000
-
+qRes resq 1
 
 section .data
 
@@ -21,9 +21,20 @@ qwNum dq 10000000000
 section .text
 global _start
 _start:
-    mov ah, 0x0e
-    mov al, 'Q'
-    int 0x10
+    mov rax, qword [qwNum]
+    add rax, qword [bNum]
+    mov qword [qRes], rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, "BING Q"
+    mov rdx, 7
+    syscall
+;    mov dl, 'Q'
+;    mov ah, 2
+;    syscall
+;    mov ah, 0x0e
+;    mov al, 'Q'
+;    int 0x10
 
 last:
     mov rax, SYS_exit
