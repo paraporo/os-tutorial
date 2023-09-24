1434a591b8c08f106551627ff96a3b4d26872a6f
Ahmet Erdem <56089895+paraporo@users.noreply.github.com>
2023-09-24 14:28:47 +0300

    test.asm

--- 99-sandbox/2/4.asm
+++ 99-sandbox/2/4.asm
@@ -7,13 +7,19 @@ section .data
 EXIT_SUCCESS equ 0
 SYS_exit equ 60
 
-qVal dq 0x123456789ABCDEF0
+dVal1 dd 0xffffffff
+
+qVal1 dq 0x123456789ABCDEF0
+qVal2 dq 123
 
 section .text
 global _start
 _start:
-	mov rax, [qVal]
-
+	mov rax, qword [qVal1]
+	mov eax, dword [qVal1]
+	mov ax, word [qVal1]
+	mov al, byte [qVal1]
+	lea rbx, qword [qVal1]
 last:
 	mov rax, EXIT_SUCCESS
 	mov rdi, SYS_exit 
